cbd80a63af0fe715802f21656d59b0c7664c25dd
msiism99 <msiism99@naver.com>
2026-04-10 17:19:44 +0900

    Update 004.py

--- bdq/004.py
+++ bdq/004.py
@@ -13,7 +13,7 @@ else:
       r.test_point_no,r.coordinate_x,r.coordinate_y,r.value_x,r.value_y,
       r.mrc_x_valn,r.mrc_y_valn
     FROM ees_ds_eai.apc_nautil_dat_rawdata r
-    WHERE r.impala_insert_time >= now() - interval {days+1} days
+    WHERE r.impala_insert_time >= now() - interval {days+10} days
       AND r.db_user = '{db_user}'
       AND r.lot_transn_seq IN ({lot_transn_seq_oco_str})
       AND r.data_kind IN ('RAWDATA', 'TESTDATA', 'PERSHOT')
@@ -22,9 +22,20 @@ else:
     
     print("NAUTIL_DAT_RAWDATA 데이터 조회 시작...")
     df_rawdata_oco = bdq.getData(sql_rawdata_oco)
+    df_rawdata_oco['slot_id'] = df_rawdata_oco['slot_id'].astype(int)
+    df_rawdata_oco = df_rawdata_oco.merge(
+        df_lotinfo_oco[['lot_transn_seq', 'slotid']].drop_duplicates(),
+        left_on=['lot_transn_seq', 'slot_id'],   # df_rawdata_adi 기준 컬럼
+        right_on=['lot_transn_seq', 'slotid'],   # df_lotinfo_adi 기준 컬럼
+        how='inner'
+    ).reset_index(drop=True)
+
     print(f"✅ RAWDATA_OCO rows: {len(df_rawdata_oco)}")
     print(f"data_kind 분포:")
     print(df_rawdata_oco['data_kind'].value_counts())
+    print(f"WF 분포:")
+    print(df_rawdata_oco[['lot_transn_seq','slot_id']].value_counts())
+    
     print(df_rawdata_oco.head())
 df_rawdata_oco.to_excel('NAUTIL_DAT_RAWDATA_OCO.xlsx')
 
@@ -51,8 +62,18 @@ else:
     
     print("NAUTIL_DAT_RAWDATA 데이터 조회 시작...")
     df_rawdata_adi = bdq.getData(sql_rawdata_adi)
+    df_rawdata_adi['slot_id'] = df_rawdata_adi['slot_id'].astype(int)
+    df_rawdata_adi = df_rawdata_adi.merge(
+        df_lotinfo_adi[['lot_transn_seq', 'slotid']].drop_duplicates(),
+        left_on=['lot_transn_seq', 'slot_id'],   # df_rawdata_adi 기준 컬럼
+        right_on=['lot_transn_seq', 'slotid'],   # df_lotinfo_adi 기준 컬럼
+        how='inner'
+    ).reset_index(drop=True)
+
     print(f"✅ RAWDATA_OCO rows: {len(df_rawdata_adi)}")
     print(f"data_kind 분포:")
     print(df_rawdata_adi['data_kind'].value_counts())
+    print(f"WF 분포:")
+    print(df_rawdata_adi[['lot_transn_seq','slot_id']].value_counts())
     print(df_rawdata_adi.head())
 df_rawdata_adi.to_excel('NAUTIL_DAT_RAWDATA_ADI.xlsx')
